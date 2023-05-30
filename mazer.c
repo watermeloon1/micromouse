f93d41579aef2c350f2000585bf4f227906c68e0
WaterMeloon <ifkovics.barnabas@gmail.com>
2023-05-30 09:53:04 +0200

    Add mouse

--- mazer.c
+++ mazer.c
@@ -19,6 +19,20 @@
 #define SOUTH (uint8_t)0x04
 #define WEST  (uint8_t)0x08
 
+enum orient {
+    north,
+    east,
+    south,
+    west,
+    size
+};
+
+typedef struct {
+    uint32_t x;
+    uint32_t y;
+    enum orient orient;
+} Mouse;
+
 uint8_t global_maze[256];
 
 void read_bytes(FILE* file, void* buffer, const size_t buffer_capacity) {
@@ -87,11 +101,10 @@ void SDL_RenderDrawCustomRect(SDL_Renderer* renderer, uint32_t start_x, uint32_t
 
 void maze_draw_cell(SDL_Renderer* renderer, uint8_t row, uint8_t col) {
 
-    uint8_t index = row * COL + col;
-    uint8_t wall_data = global_maze[index];
+    uint8_t wall_data = global_maze[row * COL + col];
     
     uint32_t topleft_x = (PAD) + row * (CELL + 1);
-    uint32_t topleft_y = (HIGHT - PAD - 1) - (col + 1) * (CELL + 1);
+    uint32_t topleft_y = (PAD + 16 * CELL + 16) - (col + 1) * (CELL + 1);
 
     // WEST 
     if (0 <= wall_data - WEST) {
@@ -139,12 +152,52 @@ void maze_draw_cell(SDL_Renderer* renderer, uint8_t row, uint8_t col) {
   line width = 1px (17) 
   cell dim = 25px (16)
 */   
-void maze_draw(SDL_Renderer* renderer) {
+void SDL_RenderDrawMaze(SDL_Renderer* renderer) {
     for (unsigned int i = 0; i < ROW; ++i) {
         for (unsigned int j = 0; j < COL; ++j) {
             maze_draw_cell(renderer, i, j);
         }
     }
+}
+
+void rotatePoint(float* x, float* y, float pivot_x, float pivot_y) {
+    float dx = *x - pivot_x;
+    float dy = *y - pivot_y;
+
+    *x = pivot_x - dy;
+    *y = pivot_y + dx;
+}
+
+// transforms between SDL context and maze context
+void SDL_RenderDrawMouse(SDL_Renderer* renderer, Mouse* mouse) {
+    
+    uint32_t pos_x = PAD + (mouse -> x + 1) + mouse -> x * CELL + CELL / 2;
+    uint32_t pos_y = PAD + (ROW - mouse -> y) +  (ROW - (mouse -> y + 1)) * CELL + CELL / 2;
+
+    float v_mouse1[] = { pos_x, pos_y - 5 };
+    float v_mouse2[] = { pos_x - 5,  pos_y + 5 };
+    float v_mouse3[] = { pos_x + 5, pos_y + 5 };
+
+    for (int i = 0; i < mouse -> orient; ++i) {
+        rotatePoint(&v_mouse1[0], &v_mouse1[1], (float)pos_x, (float)pos_y);
+        rotatePoint(&v_mouse2[0], &v_mouse2[1], (float)pos_x, (float)pos_y);
+        rotatePoint(&v_mouse3[0], &v_mouse3[1], (float)pos_x, (float)pos_y);
+    }    
+
+    SDL_Vertex vertex_1 = { {v_mouse1[0], v_mouse1[1]}, { 255, 0, 0, 255 }, { 1, 1 } };
+    SDL_Vertex vertex_2 = { {v_mouse2[0], v_mouse2[1]}, { 255, 0, 0, 255 }, { 1, 1 } };
+    SDL_Vertex vertex_3 = { {v_mouse3[0], v_mouse3[1]}, { 255, 0, 0, 255 }, { 1, 1 } };
+
+    // Put them into array
+    SDL_Vertex vertices[] = {
+        vertex_1,
+        vertex_2,
+        vertex_3
+    };
+
+    SDL_RenderGeometry(renderer, NULL,
+                       vertices, sizeof(vertices) / sizeof(SDL_Vertex),
+                       NULL, 0);
 }
 
 int main() {
@@ -168,13 +221,12 @@ int main() {
     printf("SDL init had no errors!\n");
     maze_read_file("binary_mazefiles/japan2007eq.maz");
     assert(maze_valid());
-
-    SDL_Rect maze_border;
-    maze_border.x = PAD;
-    maze_border.y = PAD;
-    maze_border.w = 17 * WALL + 16 * CELL;
-    maze_border.h = 17 * WALL + 16 * CELL;
     
+    Mouse* mouse = malloc(sizeof(Mouse));
+    mouse -> x = 0;
+    mouse -> y = 0;
+    mouse -> orient = north;
+        
     bool quit = false;
     while(!quit) {
         SDL_Event event;
@@ -188,10 +240,11 @@ int main() {
         SDL_RenderClear(renderer);
 
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE); 
-        SDL_RenderDrawRect(renderer, &maze_border);
+        SDL_RenderDrawMaze(renderer);
 
-        maze_draw(renderer);
-
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, SDL_ALPHA_OPAQUE); 
+        SDL_RenderDrawMouse(renderer, mouse);
+        
         SDL_RenderPresent(renderer);
     }
 
